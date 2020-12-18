ce509c0e7c96d2db5cb565cc9f96a7263b007d6d
Ishita Gupta <53005952+IshitaGupta27@users.noreply.github.com>
2020-12-18 23:48:53 +0530

    Update footer.php

--- include/footer.php
+++ include/footer.php
@@ -12,8 +12,8 @@
 							</h1>
 
 							<div class="details">
-								 <p> 92 300 1234567 <i class="fa fa-phone"></i>
-								exceptionalprogrammers@gmail.com <i class="fa fa-envelope"></i>  </p>
+								 <p> 9211879623 <i class="fa fa-phone"></i>
+								blooddonation@gmail.com <i class="fa fa-envelope"></i>  </p>
 							</div>
 
 					</div>
