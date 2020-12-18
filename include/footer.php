39b79642e793acc4b28d53c0873aab35cb58fef1
Ishita Gupta <53005952+IshitaGupta27@users.noreply.github.com>
2020-12-18 23:52:37 +0530

    Update footer.php

--- include/footer.php
+++ include/footer.php
@@ -4,13 +4,6 @@
 					<div class="col-md-6 offset-md-3">
 						<h1 class="text-center">Contact Us</h1>
 						<hr class="white-bar">
-
-							<h1 class="text-center">
-								<a  target="_blank" href="https://www.facebook.com/exceptionalprogrammers"><i style="color: #fff;" class="fab fa-facebook-square"></i></a>
-
-								<a target="_blank" href="https://www.youtube.com/exceptionalprogrammers"><i style="color: #fff;" class="fab fa-youtube-square"></i></a> 
-							</h1>
-
 							<div class="details">
 								 <p> 9211879623 <i class="fa fa-phone"></i>
 								blooddonation@gmail.com <i class="fa fa-envelope"></i>  </p>
